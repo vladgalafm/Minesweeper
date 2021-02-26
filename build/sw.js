04f3c15b621e471abb9b72a5c1dde557a6cdf6e6
Vladyslav Halushkin <v.halushkin@gmail.com>
2021-02-26 14:24:14 +0200

    build v0.5

---
+++ build/sw.js
@@ -0,0 +1,48 @@
+const APP_PREFIX = 'hv-mines_';
+const VERSION = 'v0.5';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const URLS = [
+    '/Minesweeper/',
+    '/Minesweeper/index.html'
+];
+
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                return request;
+            } else {
+                return fetch(e.request);
+            }
+        })
+    )
+});
+
+// Cache resources
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(URLS);
+        })
+    )
+});
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            const cacheWhitelist = keyList.filter(function(key) {
+                return key.indexOf(APP_PREFIX);
+            });
+
+            cacheWhitelist.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    return caches.delete(keyList[i]);
+                }
+            }))
+        })
+    )
+});
